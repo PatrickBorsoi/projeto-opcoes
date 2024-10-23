df64b4948e89106c16e7ebe98de033817d02741d
Patrick Perez <patrickperez1698@gmail.com>
2024-10-23 01:06:01 -0300

    correcao de bug

--- src/etl.py
+++ src/etl.py
@@ -4,6 +4,12 @@ import os
 import glob
 import shutil
 from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+
+
 def verifcar_dia():
     data = datetime.now()
     data_formatada = data.strftime("%d-%m-%Y")
@@ -20,16 +26,28 @@ def mover_arquivos(path_downloads: str, path_pasta_padrao_dos_arquivos: str, pri
 
 
 def ler_arquivos(path_pasta_padrao_dos_arquivos: str) -> pd.DataFrame:
+    # Buscar todos os arquivos Excel na pasta
     arquivos_excel = glob.glob(os.path.join(path_pasta_padrao_dos_arquivos, '*.xlsx'))
-    df_list: list = [pd.read_excel(arquivo,skiprows=1,engine='openpyxl') for arquivo in arquivos_excel]
-    df_tabela_total: list = pd.concat(df_list, ignore_index=True)
-    df_tabela_total = pd.DataFrame(df_tabela_total)
-    df_tabela_total.columns= df_tabela_total.columns.str.strip()
-    # df_tabela_total['Strike'] = df_tabela_total['Strike'].combine_first(df_tabela_total['Strike'])
-    df_tabela_total['Strike'] = df_tabela_total['Strike']
+    
+    # Ler todos os arquivos Excel e armazenar em uma lista de DataFrames
+    df_list = [pd.read_excel(arquivo, skiprows=1, engine='openpyxl') for arquivo in arquivos_excel]
+    
+    # Concatenar todos os DataFrames em um único DataFrame
+    df_tabela_total = pd.concat(df_list, ignore_index=True)
+    
+    df_tabela_total.columns = df_tabela_total.columns.str.strip()
+        
+    cols = pd.Series(df_tabela_total.columns)
+    for dup in cols[cols.duplicated()].unique():  # Identifica colunas duplicadas
+        cols[cols[cols == dup].index.values.tolist()] = [dup + '_' + str(i) if i != 0 else dup for i in range(sum(cols == dup))]
+    df_tabela_total.columns = cols
 
-
+    df_tabela_total['Strike_Unificado'] = df_tabela_total['Strike'].combine_first(df_tabela_total['Strike_1'])
+    df_tabela_total = df_tabela_total.drop(columns=['Strike', 'Strike_1'])
+    print(df_tabela_total)
+    
     return df_tabela_total
+
 
 
 def remover_arquivos(path_pasta_padrao_dos_arquivos: str, primeira_palavra):
@@ -63,3 +81,22 @@ def transformacao(df: pd.DataFrame):
     # Remova as linhas onde a coluna 'Núm. de Neg.' é menor que 50
     df_final = df_final[df_final['Núm. de Neg.'] >= 50]
     return df_final
+
+
+def extracao_de_planilhas():
+    driver = webdriver.Chrome()
+    driver.get('https://opcoes.net.br/opcoes/bovespa')
+    opcoes: list = driver.find_elements(By.XPATH, '//*[@id="grade-opcoes"]/div[1]/div[1]/select[2]/option')
+
+    lista_de_ativos: list = []
+
+    for nome_ativo in opcoes[1:]:
+        time.sleep(1)
+        nome_ativo.click()
+        lista_de_ativos.append(nome_ativo.text)
+        time.sleep(2)
+        botao_exportar_para_excel = driver.find_element(By.XPATH, '//*[@id="tblListaOpc_wrapper"]/div[1]/div[2]/div/button[2]')
+        time.sleep(1)
+        botao_exportar_para_excel.click()
+
+    driver.quit()
--- src/pipeline_opcoes.py
+++ src/pipeline_opcoes.py
@@ -11,22 +11,7 @@ from etl import *
 from bs4 import BeautifulSoup
 import requests
 
-# driver = webdriver.Chrome()
-# driver.get('https://opcoes.net.br/opcoes/bovespa')
-# opcoes: list = driver.find_elements(By.XPATH, '//*[@id="grade-opcoes"]/div[1]/div[1]/select[2]/option')
-
-# lista_de_ativos: list = []
-
-# for nome_ativo in opcoes[1:]:
-#     time.sleep(1)
-#     nome_ativo.click()
-#     lista_de_ativos.append(nome_ativo.text)
-#     time.sleep(2)
-#     botao_exportar_para_excel = driver.find_element(By.XPATH, '//*[@id="tblListaOpc_wrapper"]/div[1]/div[2]/div/button[2]')
-#     time.sleep(1)
-#     botao_exportar_para_excel.click()
-
-
+extracao_de_planilhas()
 
 datas = verifcar_dia()
 
@@ -34,21 +19,17 @@ data_atual = datas['dia_atual_formatado']
 
 
 path_download = os.path.expanduser("~/Downloads")
-path_pasta_opcoes = os.path.expanduser("C:/Users/Patrick/Desktop/workspace/pipeline_opcoes/data/22-10-2024_arquivo todas opções.xlsx")
+path_pasta_opcoes = os.path.expanduser("C:/Users/Patrick/Desktop/workspace/pipeline_opcoes/data")
 primeira_palavra = 'Opções'
 
-# mover_arquivos(path_downloads=path_download,path_pasta_padrao_dos_arquivos=path_pasta_opcoes, primeira_palavra=primeira_palavra)
+mover_arquivos(path_downloads=path_download,path_pasta_padrao_dos_arquivos=path_pasta_opcoes, primeira_palavra=primeira_palavra)
 
 df_total = ler_arquivos(path_pasta_padrao_dos_arquivos=path_pasta_opcoes)
 
-
-df_total['Strike'] = df_total['Strike']
-
-print(df_total)
-# remover_arquivos(path_pasta_padrao_dos_arquivos=path_pasta_opcoes, primeira_palavra=primeira_palavra)
+remover_arquivos(path_pasta_padrao_dos_arquivos=path_pasta_opcoes, primeira_palavra=primeira_palavra)
 
 
-# df_total = transformacao(df=df_total)
+df_total = transformacao(df=df_total)
 
 tipo_do_arquivo = input('Digite o arquivo (csv ou xlsx)')
 
